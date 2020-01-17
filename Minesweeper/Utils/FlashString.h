d8509ca858a19f57b6e7f78a17f94e88b5f2b22d
Joshua Cogliati <jrincayc@users.noreply.github.com>
2020-01-17 15:58:01 -0700

    Fix use of contexpr with reinterpret_cast (#70)
    Somehow I missed this on my first attempt. (See #65 and #68.)
    Thank you to @jrincayc for pointing this out.
    
    Co-authored-by: Pharap <2933055+Pharap@users.noreply.github.com>
    Co-authored-by:  Joshua Cogliati <jrincayc@users.noreply.github.com>

--- Minesweeper/Utils/FlashString.h
+++ Minesweeper/Utils/FlashString.h
@@ -22,17 +22,17 @@
 
 using FlashString = const __FlashStringHelper *;
 
-constexpr inline FlashString AsFlashString(const char * flashString)
+inline FlashString AsFlashString(const char * flashString)
 {
 	return reinterpret_cast<FlashString>(flashString);
 }
 
-constexpr inline FlashString AsFlashString(const unsigned char * flashString)
+inline FlashString AsFlashString(const unsigned char * flashString)
 {
 	return reinterpret_cast<FlashString>(flashString);
 }
 
-constexpr inline FlashString AsFlashString(const signed char * flashString)
+inline FlashString AsFlashString(const signed char * flashString)
 {
 	return reinterpret_cast<FlashString>(flashString);
 }
