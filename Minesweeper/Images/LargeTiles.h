10ff04e328cc66a999e0d7f650fbd9545628b613
Pharap <2933055+Pharap@users.noreply.github.com>
2021-07-20 13:01:53 +0100

    Improve citations (#78)

--- Minesweeper/Images/LargeTiles.h
+++ Minesweeper/Images/LargeTiles.h
@@ -17,8 +17,8 @@
 //
 
 //
-// Large tiles created by Pharap (https://github.com/Pharap)
-// Penguin large tile created by Vampirics (https://github.com/vampirics)
+// Number tiles, Classic and Modern Mine tiles created by Pharap (https://github.com/Pharap)
+// Cactus, Penguin, and Turtle Mine tiles created by Vampirics (https://github.com/vampirics)
 //
 
 namespace Images
